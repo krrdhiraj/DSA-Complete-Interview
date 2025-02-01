954933f8a78fe45f2c11f142d624996a485b84ba
Dhiraj kumar <chiku930400@gmail.com>
2025-02-01 18:33:37 +0530

    Time: 1 ms (37.58%), Space: 43.1 MB (84.31%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i = 1; i<n; i++){
             int first = nums[i-1] %2;
             int second = nums[i] %2;
-            if((first == 0 && second == 0) || (first != 0 && second != 0))
+            if(first == second)
                 return false;
             
         }
