7a2052c6c1ea461484c0ce1861a9569a1fd805a8
Dhiraj kumar <chiku930400@gmail.com>
2025-02-01 22:33:11 +0530

    Time: 8 ms (50.07%), Space: 126 MB (97.27%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
-    
+    bool specialBinarySearch(int s, int e, vector<int> &vi){
+        int l = 0, r = vi.size()-1;
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(s > vi[mid]){
+                l = mid + 1;
+            }else if(e < vi[mid]){
+                r = mid - 1;
+            }else{
+                return true; // violating index mil gaya
+            }
+        }
+        return false;
+    }
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        vector<int> preSm(n,0); // for storing the violating indices.
+        vector<int> violateIndx; // for storing the violating indices.
+        
         for(int i = 1; i<n; i++){
-           if((nums[i-1]%2) == (nums[i] % 2)){ // found violating index
-                preSm[i] = preSm[i-1] + 1;
-           }else{
-                preSm[i] = preSm[i-1];
+           if((nums[i-1]%2) == (nums[i] % 2)){ // found violating index then store it.
+                violateIndx.push_back(i);
            }
         }
         int m = queries.size();
@@ -16,7 +28,7 @@ public:
         int i = 0;
         for(auto &v : queries){
             int s = v[0], e = v[1];
-            if((preSm[e]-preSm[s]) == 0)
+            if(!specialBinarySearch(s+1,e, violateIndx))
                 ans[i] = true;
             i++;
         }
