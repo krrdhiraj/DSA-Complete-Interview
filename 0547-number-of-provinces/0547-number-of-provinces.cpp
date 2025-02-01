472067c9fe3b8ad999a8cd33e9f61c8595b6eb6a
Dhiraj kumar <chiku930400@gmail.com>
2025-02-02 02:42:57 +0530

    Time: 3 ms (44.3%), Space: 19.1 MB (97.31%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    void DFS(unordered_map<int, vector<int>> &adj,int u, vector<bool> &visited){
+    void DFS(vector<vector<int>> &isConnected,int u, vector<bool> &visited){
         visited[u] = true;
-        for(int &v : adj[u]){
-            if(!visited[v]){
-                DFS(adj, v, visited);
+       // visit neighbours 
+        for(int v = 0; v < isConnected.size(); v++){
+            if(!visited[v] && isConnected[u][v] == 1){
+                DFS(isConnected, v, visited);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        unordered_map<int, vector<int>> adj;
         vector<bool> visited(n, false);
-        // make adjacency list
-        for(int u = 0; u < n; u++){
-            for(int v = 0; v < n; v++){
-                if(isConnected[u][v] == 1){ // connected haai
-                    adj[u].push_back(v);
-                    adj[v].push_back(u);
-                }
-            }
-        }
+       
         int provinces = 0;
         // call DFS and count provinces
         for(int i = 0; i<n; i++){
             if(!visited[i]){
                 provinces++;
-                DFS(adj, i, visited);
+                DFS(isConnected, i, visited);
             }
         }
         return provinces;
