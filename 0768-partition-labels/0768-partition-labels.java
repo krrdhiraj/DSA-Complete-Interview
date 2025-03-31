0b5a79f731669655a7c313c3b2e2f647daf87a29
Dhiraj kumar <chiku930400@gmail.com>
2025-03-31 18:31:25 +0530

    Time: 4 ms (92.9%), Space: 42.2 MB (70.63%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+         int n = s.length();
+
+        List<Integer> ans = new ArrayList<>();
+
+       int[] mp = new int[26]; // storing last index of a char
+        for(int i = 0; i<n; i++){
+            mp[(s.charAt(i)-'a')] = i;
+        }
+
+        int i = 0;
+        int start = 0, end = 0;
+        while(i < n){
+            end = Math.max(end, mp[(s.charAt(i)-'a')]);
+            if( i == end){
+                ans.add(end - start + 1);
+                start = end + 1;
+            }
+            i++;
+        }
+        return ans;
+    }
+}
