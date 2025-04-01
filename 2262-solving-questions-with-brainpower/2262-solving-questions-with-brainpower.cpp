fe7277e497e323d3c75d7a2b9498d50761619a8b
Dhiraj kumar <chiku930400@gmail.com>
2025-04-02 01:12:46 +0530

    Time: 32 ms (8.71%), Space: 211.6 MB (5.05%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    int n;
-    long long solveRecur(vector<vector<int>> & ques, int i, vector<long long>& dp){
-        if(i >= n)
-            return 0;
-        if(dp[i] != -1)
-            return dp[i];
-        long long taken = ques[i][0] + solveRecur(ques, i + ques[i][1] + 1, dp);
-        long long not_taken = solveRecur(ques, i+1, dp);
-
-        return dp[i] = max(taken, not_taken);
-    }
+   
     long long mostPoints(vector<vector<int>>& questions) {
-       n = questions.size();
-        vector<long long> dp(n, -1);
-       return solveRecur(questions, 0, dp);
+       int n = questions.size();
+       if(n == 0){
+        return questions[0][0];
+       }
+        vector<long long> dp(200001, 0);
+
+        for(int i = n-1; i>= 0; i--){
+            dp[i] = max(questions[i][0] + dp[i + questions[i][1] + 1], dp[i+1]);
+        }
+
+        return dp[0];
     }
 };
