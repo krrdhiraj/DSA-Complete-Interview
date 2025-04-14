814f56381483d8ad64f81422c267a45a41f6f771
Dhiraj kumar <chiku930400@gmail.com>
2025-04-15 02:19:19 +0530

    Time: 10 ms (77.06%), Space: 41.3 MB (59.63%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.java
+++ 1656-count-good-triplets/1656-count-good-triplets.java
@@ -4,11 +4,13 @@ class Solution {
         int n = arr.length;
         for(int i = 0; i<n; i++){
             for(int j = i+1; j < n ; j++){
+                 int diff1 = Math.abs(arr[i]-arr[j]);
+                 if(diff1 > a)
+                    continue;
                 for(int k = j+1; k < n ; k++){
-                    int diff1 = Math.abs(arr[i]-arr[j]);
                     int diff2 = Math.abs(arr[j]-arr[k]);
                     int diff3 = Math.abs(arr[i] - arr[k]);
-                    if(diff1 <= a && diff2 <= b && diff3 <= c){
+                    if(diff2 <= b && diff3 <= c){
                         ans++;
                     }
                 }
