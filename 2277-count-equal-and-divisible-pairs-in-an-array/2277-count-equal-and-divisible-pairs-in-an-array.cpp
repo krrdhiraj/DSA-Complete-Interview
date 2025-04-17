a59c2acedf7f44b39fdb256eb820477363898d50
Dhiraj kumar <chiku930400@gmail.com>
2025-04-17 22:20:10 +0530

    Time: 0 ms (100%), Space: 15.4 MB (91.08%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
         int n = nums.size();
-        int cnt = 0;
+        int ans = 0;
         for(int i = 0; i<n; i++){
-            for(int j = i + 1; j<n; j++){
-                if(nums[j]==nums[i] && ((j*i) % k == 0)){
-                    cnt++;
+            for(int j = i+1; j<n; j++){
+                if(nums[i] == nums[j] && (i*j) % k == 0){
+                    ans++;
                 }
             }
         }
-        return cnt;
+        return ans;
     }
 };
