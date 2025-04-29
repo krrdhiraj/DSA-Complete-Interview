4cfdeb1a635bdbd43cb7954944fd8feef72c15ee
Dhiraj kumar <chiku930400@gmail.com>
2025-04-30 02:03:07 +0530

    Time: 42 ms (81.84%), Space: 47.3 MB (79.78%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
@@ -1,24 +1,34 @@
 class Solution {
-     public int slidingWindow(int[] nums, int k) {
+     
+    public int subarraysWithKDistinct(int[] nums, int k) {
         int n = nums.length;
         int ans = 0;
         Map<Integer,Integer> mp = new HashMap<>();
         int i = 0, j = 0;
+        int lastIdxOfI = 0;
         while(j < n){
             mp.put(nums[j], mp.getOrDefault(nums[j],0) + 1);
+            // invalid subarray or window
             while(mp.size() > k){
                 mp.put(nums[i], mp.get(nums[i])-1);
                 if(mp.get(nums[i]) == 0){
                     mp.remove(nums[i]);
                 }
                 i++;
+                lastIdxOfI = i;
             }
-            ans += (j-i+1);
+
+            // shrink the window : find min length valid subarr
+            while(mp.get(nums[i]) > 1){
+                mp.put(nums[i], mp.get(nums[i])-1);
+                i++;
+            }
+
+            // count the valid subarr 
+            if(mp.size() == k)
+                ans += (i - lastIdxOfI +1);
             j++;
         }
         return ans;
-    }
-    public int subarraysWithKDistinct(int[] nums, int k) {
-        return slidingWindow(nums, k) - slidingWindow(nums, k-1);
     }
 }
