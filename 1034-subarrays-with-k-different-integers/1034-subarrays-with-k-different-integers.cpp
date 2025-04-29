1f7ec8b017de89bf4228a1d1862ad829a9f18883
Dhiraj kumar <chiku930400@gmail.com>
2025-04-30 01:59:12 +0530

    Time: 30 ms (94.96%), Space: 47.8 MB (94.66%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    int slidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+       int n = nums.size();
         int ans = 0;
         unordered_map<int,int> mp;
         int i = 0, j = 0;
+        int lastIdxOfI = 0;
         while(j < n){
             mp[nums[j]]++;
+
+            // invalid subarray
             while(mp.size() > k){
                 mp[nums[i]]--;
                 if(mp[nums[i]] == 0){
                     mp.erase(nums[i]);
                 }
                 i++;
+                lastIdxOfI = i;
             }
-            ans += (j-i+1);
+
+            //shrink the window : jb tak nums[i] ki freq 1 nhi hoti means shrink till get min subarr length
+            while(mp[nums[i]] > 1){
+                mp[nums[i]]--;
+                i++;
+            }
+            // calculate the no of subarr
+            if(mp.size() == k){
+                ans += (i - lastIdxOfI + 1);
+            }
             j++;
         }
         return ans;
-    }
-    int subarraysWithKDistinct(vector<int>& nums, int k) {
-        int subarrWithKDiffEle = slidingWindow(nums,k);
-        int subarrWithLessKDiff = slidingWindow(nums, k-1);
-
-        return subarrWithKDiffEle - subarrWithLessKDiff;
     }
 };
