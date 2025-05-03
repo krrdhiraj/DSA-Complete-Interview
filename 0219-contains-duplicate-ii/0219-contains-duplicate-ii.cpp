4462df2a92943840add8e345d501dd82d94f9df1
Dhiraj kumar <chiku930400@gmail.com>
2025-05-04 03:34:18 +0530

    Time: 65 ms (63.7%), Space: 98.5 MB (42.16%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -3,16 +3,12 @@ public:
     // Brute force approach TC{O(n^2)}
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         int n = nums.size();
-        unordered_map<int,vector<int>> mp;
+        unordered_map<int,int> mp;
         for(int i = 0; i<n; i++){
-            mp[nums[i]].push_back(i);
-        }
-        for(auto &it : mp){
-            vector<int> v = it.second;
-            for(int i = 0; i<v.size()-1; i++){
-                if(abs(v[i]-v[i+1]) <= k)
-                    return true;
-            }
+           if(mp.count(nums[i])&& i-mp[nums[i]] <= k){
+            return true;
+           }
+           mp[nums[i]] = i;
         }
         return false;
     }
