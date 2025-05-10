733dfacdc3cb1e04b66ee32750c2cb3bc7988758
Dhiraj kumar <chiku930400@gmail.com>
2025-05-10 21:09:32 +0530

    Time: 126 ms (76.61%), Space: 135.3 MB (73.56%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -8,20 +8,16 @@ public:
         int zero1 = 0, zero2 = 0;
 
         for(int num : nums1){
-            sm1 += num;
-            if(num == 0){
-                zero1++;
-            }
+            sm1 += num == 0? 1 : num;
+            zero1 += num == 0 ? 1 : 0;
         }
         for(int num : nums2){
-            sm2 += num;
-            if(num == 0){
-                zero2++;
-            }
+            sm2 += num == 0 ? 1 : num;
+            zero2 += num == 0 ? 1 : 0;
         }
-        if((zero1 == 0 && sm1 < sm2 + zero2) || (zero2 == 0 && sm2 < sm1+zero1)){
+        if((zero1 == 0 && sm1 < sm2) || (zero2 == 0 && sm2 < sm1)){
             return -1;
         } 
-        return max(zero1+sm1, zero2 + sm2);
+        return max(sm1, sm2);
     }
 };
