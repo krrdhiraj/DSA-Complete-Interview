909fe559fafe8097ef83740a78f5401def1e183b
Dhiraj kumar <chiku930400@gmail.com>
2025-05-13 01:47:27 +0530

    Time: 395 ms (13.92%), Space: 13.9 MB (49.75%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -4,33 +4,13 @@ public:
         int n = digits.size();
         set<int> ans;
         for(int i = 0; i<n; i++){
-            for(int j = i+1; j <n ; j++){
-                for(int k = j+1; k<n; k++){
-                    if(digits[i] != 0){
-                        int num1 = digits[i]*100 + digits[j]*10+ digits[k];
-                        int num2 = digits[i]*100 + digits[k]*10+ digits[j];
-                        if(num1 % 2 == 0){
-                            ans.insert(num1);
-                        }if(num2 % 2 == 0){
-                            ans.insert(num2);
-                        }
-                    }if(digits[j] != 0){
-                        int num1 = digits[j]*100 + digits[i]*10+ digits[k];
-                        int num2 = digits[j]*100 + digits[k]*10+ digits[i];
-                        if(num1 % 2 == 0){
-                            ans.insert(num1);
-                        }if(num2 % 2 == 0){
-                            ans.insert(num2);
-                        }
-                    }
-                    if(digits[k] != 0){
-                        int num1 = digits[k]*100 + digits[i]*10+ digits[j];
-                        int num2 = digits[k]*100 + digits[j]*10+ digits[i];
-                        if(num1 % 2 == 0){
-                            ans.insert(num1);
-                        }if(num2 % 2 == 0){
-                            ans.insert(num2);
-                        }
+            for(int j = 0; j <n ; j++){
+                for(int k = 0; k<n; k++){
+                    if(i == j || j == k || i == k)
+                        continue;
+                    int num = digits[i]*100 + digits[j]*10 + digits[k];
+                    if(num > 99 && num % 2 == 0){
+                        ans.insert(num);
                     }
                 }
             }
