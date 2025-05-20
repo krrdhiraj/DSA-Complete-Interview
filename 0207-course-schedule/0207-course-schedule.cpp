00b2be898562a5f964a8241359043627daff1db6
Dhiraj kumar <chiku930400@gmail.com>
2025-05-21 01:08:46 +0530

    Time: 5 ms (61.17%), Space: 19.9 MB (52.24%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    bool topoSortBFS(unordered_map<int, vector<int>>& adj, int n, vector<int>& indegree){
-        queue<int> que; 
-        int cnt =0; // keep track of how many nodes(vertices) i've visited
-        // push all the vertices into que whose indegree = 0
-        for(int i = 0; i<n; i++){
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses,0);
+        unordered_map<int, vector<int>> adj;
+        
+        for(auto &preq : prerequisites){
+            int u = preq[1];
+            int v = preq[0];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        
+        queue<int> que;
+        int cnt = 0;
+        for(int i = 0; i<numCourses; i++){
             if(indegree[i] == 0){
                 que.push(i);
                 cnt++;
             }
         }
-        // apply Khan's Algorithm : to find cycle in graph
         while(!que.empty()){
             int u = que.front();
             que.pop();
+
             for(int &v : adj[u]){
                 indegree[v]--;
                 if(indegree[v] == 0){
@@ -21,22 +31,8 @@ public:
                     que.push(v);
                 }
             }
-        }  
-        return cnt == n;
-    }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = prerequisites.size();
-        vector<int> indegree(numCourses, 0);
-        unordered_map<int, vector<int>> adj;
-        // adjacency list
-        for(auto &preq : prerequisites){
-            int u = preq[1];
-            int v = preq[0];
-            adj[u].push_back(v);
-            // calculating indegree
-            indegree[v]++;
         }
-        //  cycle is present , means not possible to complete all the courses.
-        return topoSortBFS(adj, numCourses, indegree);
+
+        return cnt == numCourses;
     }
 };
