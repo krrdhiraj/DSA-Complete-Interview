b0465f16eb52914be673ac85bb0ceab6ea5049d6
Dhiraj kumar <chiku930400@gmail.com>
2025-05-21 01:43:08 +0530

    Time: 4 ms (30.08%), Space: 19.3 MB (91.76%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-    void DFS(unordered_map<int, vector<int>> &adj, int u, vector<bool> &visited){
-        if(visited[u]){
-            return ;
-        }
+    void DFS(vector<vector<int>>& adj, int u, vector<bool> &visited){
+        
         visited[u] = true;
-        for(int &v : adj[u]){
-            if(!visited[v]){
+        for(int v = 0 ; v < adj[u].size(); v++){
+            if(!visited[v] && adj[u][v] == 1){
                 DFS(adj, v, visited);
             }
-        }
+        } 
+        
     }
-    int findCircleNum(vector<vector<int>>& isConnected) {
+    int findCircleNum(vector<vector<int>>& adj) {
         // make adjancecy list
-        int n = isConnected.size();
-        unordered_map<int, vector<int>> adj;
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<isConnected[0].size(); j++){
-                if(isConnected[i][j] == 1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
+        int n = adj.size();
 
         vector<bool> visited(n, false);
         int countOfProvinces = 0;
