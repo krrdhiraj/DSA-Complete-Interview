13097dde0adb9037732ce5fd3c2f12f417772d8a
Dhiraj kumar <chiku930400@gmail.com>
2025-05-21 02:16:18 +0530

    Time: 8 ms (24.98%), Space: 18.4 MB (56.81%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    void DFSTopologicalSort(unordered_map<int, vector<int>>& adj, int u, vector<int>& res,  vector<bool>& visited, vector<bool>& inRecursion){
-        visited[u] = true;
-        inRecursion[u] = true;
-        for(int &v : adj[u]){
-            if(inRecursion[v] == true){
-                return;
-            }
-            if(!visited[v]){
-                DFSTopologicalSort(adj,v, res,visited , inRecursion);
-            }
-        }
-        res.push_back(u);
-        inRecursion[u] = false;
-    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        // adjacency list
-        unordered_map<int, vector<int>> adj;
         vector<int> res;
-        vector<bool> visited(numCourses, false);
-        vector<bool> inRecursion(numCourses, false);
+        unordered_map<int, vector<int>> adj;
 
-        // edge (u ---> v)
-        for(vector<int> &preq : prerequisites){
-            int v = preq[0];
+         vector<int> indegree(numCourses, 0);
+       // edges(u ---> v)
+        for(auto &preq : prerequisites){
             int u = preq[1];
+            int v = preq[0];
             adj[u].push_back(v);
+            indegree[v]++;
         }
-        // if topological sort is possible then we can complete all the courses otherwise not
-       for(int i = 0; i<numCourses; i++){
-            if(!visited[i])
-                DFSTopologicalSort(adj, i, res, visited, inRecursion);
-       }
-        reverse(begin(res), end(res));
-        if(res.size() == numCourses){
-           return res;
+        
+        queue<int> que;
+        for(int i = 0; i<numCourses; i++){
+            if(indegree[i] == 0){
+                que.push(i);
+            }
         }
-        return {};
+
+        while(!que.empty()){
+            int u = que.front();
+            que.pop();
+            res.push_back(u);
+            for(int &v : adj[u]){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    que.push(v);
+                }
+            }
+        }
+        if(res.size() != numCourses){
+            res.clear();
+        }
+        return res;
+        
     }
 };
