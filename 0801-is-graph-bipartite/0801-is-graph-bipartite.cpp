ca275140fe2e09c2a832dece655f612e20f985d1
Dhiraj kumar <chiku930400@gmail.com>
2025-05-21 23:29:57 +0530

    Time: 0 ms (100%), Space: 17.3 MB (47.83%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    bool BFSBipartite(vector<vector<int>>& adj, int u, vector<int>& color, int currColor){
-        queue<int> que;
-        que.push(u);
+    int currColor = 1;
+    bool DFS(vector<vector<int>>& adj, int u, vector<int>& color){
         color[u] = currColor;
 
-        // now traverse the graph
-        while(!que.empty()){
-            int curr = que.front();
-            que.pop();
-            for(int &v : adj[curr]){
-                if(color[v] == color[curr]) // dono node ka color same ho gya not a Bipartite
+        for(int &v : adj[u]){
+            if(color[v] == color[u]){
+                return false;
+            }
+            if(color[v] == -1){
+                currColor = 1 - color[u];
+                if(DFS(adj, v, color) == false){
                     return false;
-                // if currNode ko fill hi nhi kiya ho : toh kro
-                if(color[v] == -1){
-                    // color of v kya hoga ? jo last node ka h usse diff
-                    color[v] = 1 - color[curr];
-                    que.push(v);
                 }
             }
         }
-        return true; // Bipartite hoga
-    }
-    bool isBipartite(vector<vector<int>>& graph) {
-        int v = graph.size();
-        vector<int> color(v,-1);
+        return true;
+}
+    bool isBipartite(vector<vector<int>>& adj) {
+        int n = adj.size();
+        
+        vector<int> color(n, -1);
 
-        // traverse the graph and check
-        for(int u = 0; u < v; u++){
-            if(color[u] == -1){ // currColor = 1
-                if(BFSBipartite(graph, u, color, 1) == false)
-                    return false; // not Bipartite
+        for(int u = 0; u<n; u++){
+            if(color[u] == -1){
+                if(DFS(adj, u, color) == false)
+                    return false;
             }
         }
         return true;
