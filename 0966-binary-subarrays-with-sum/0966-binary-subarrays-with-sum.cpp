0d5ddaa1ea83562459116693d8680261d4148995
Dhiraj kumar <chiku930400@gmail.com>
2025-06-01 16:07:08 +0530

    Time: 0 ms (100%), Space: 32.7 MB (40.94%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-       
+        int i = 0, j = 0;
         int n = nums.size();
-        int cnt = 0;
-        for(int i = 0; i<n; i++){
-            int sum = 0;
-            for(int j = i; j<n; j++){
-                sum += nums[j];
-                if(sum == goal){
-                    cnt++;
+        int sum = 0, cnt = 0;
+        int cntZeros = 0;
+        while(j < n){
+            sum += nums[j];
+            
+            while(i < j && (nums[i] == 0 || sum > goal)){
+                if(nums[i] == 0){
+                    cntZeros += 1;
+                }else{
+                    cntZeros = 0;
                 }
-                if(sum > goal)
-                    break;
+                sum -= nums[i];
+                i++;
             }
+            if(sum == goal){
+                cnt += 1 + cntZeros;
+            }
+            j++;
         }
-        return cnt;
+        return cnt ;
     }
-};
+}; 
