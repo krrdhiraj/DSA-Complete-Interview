078588bb39babfe258d237a07782c029cd353ffe
Dhiraj kumar <chiku930400@gmail.com>
2025-06-07 00:40:42 +0530

    Time: 51 ms (70.99%), Space: 10.6 MB (90.98%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
+    int bestSplitPoint(string word, int n){
+        int i = 0; 
+        int j = 1;
+
+        while(j < n){
+            int k = 0;
+            while(j + k < n && word[j+k] == word[i+k]){
+                k++;
+            }
+            if(word[j+k] > word[i+k]){
+                i = j;
+                j = j+1;
+            }else{
+                j  = j + k + 1;
+            }
+        }
+        return i ;
+    }
     string answerString(string word, int numFriends) {
         int n = word.length();
 
         if(numFriends == 1)
             return word;
 
-        int remainChar = n - (numFriends - 1);
+        int possibleLen = n - (numFriends - 1);
 
         string ans ;
-        for(int i = 0; i<n; i++){
-            int possibleLength = min(remainChar, n-i);
-            ans = max(ans, word.substr(i,possibleLength));
-        }
+        int i = bestSplitPoint(word, n);
+       
+        int possibleLength = min(possibleLen, n-i);
+        ans = max(ans, word.substr(i,possibleLength));
+        
 
         return ans;
     }
