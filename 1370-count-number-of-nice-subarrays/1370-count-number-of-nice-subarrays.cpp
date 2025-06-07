6184f6472d8315b6e521dd1c0e5285356b0f0fda
Dhiraj kumar <chiku930400@gmail.com>
2025-06-07 09:49:17 +0530

    Time: 75 ms (18.97%), Space: 94.2 MB (19.4%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -4,14 +4,15 @@ public:
         int n = nums.size();
         int ans = 0; 
         unordered_map<int, int> mp;
-        mp[0] = 1;
         int oddCnt = 0;
+        mp[oddCnt] = 1;
+
         for(int i = 0; i<n; i++){
-            if(nums[i] % 2 != 0){
-                oddCnt++;
+            oddCnt += nums[i] % 2;
+            if(mp.count(oddCnt-k)){
+                ans += mp[oddCnt - k];
             }
-            mp[oddCnt]++;
-            ans += mp[oddCnt - k];
+           mp[oddCnt]++;
         }
         return ans;
     }
