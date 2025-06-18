9513872311e52e88fcee33e1c2dfc93cae7ce968
Dhiraj kumar <chiku930400@gmail.com>
2025-06-19 00:50:18 +0530

    Time: 43 ms (84.69%), Space: 119.9 MB (86.6%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
@@ -7,14 +7,9 @@ public:
         vector<vector<int>> ans;
         for(int i = 0; i<n-2; i+=3){
             if(nums[i+2]-nums[i] > k){
-                ans.clear();
-                return ans;
+                return {};
             }
-            vector<int> v;
-            v.push_back(nums[i]);
-            v.push_back(nums[i+1]);
-            v.push_back(nums[i+2]);
-            ans.push_back(v);
+            ans.push_back({nums[i], nums[i+1], nums[i+2]});
         }
         return ans;
     }
