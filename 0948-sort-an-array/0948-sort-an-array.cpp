daf16c32bb5779a9127d227bce96cb2f53add83f
Dhiraj kumar <chiku930400@gmail.com>
2025-07-11 03:24:09 +0530

    Time: 2786 ms (5.03%), Space: 74.1 MB (59.29%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
-    void merge(vector<int>&nums, int low, int mid, int high){
-        int i = low, j = mid+1;
-        vector<int> temp;
-        while(i <= mid && j <= high){
-            if(nums[i] <= nums[j]){
-                temp.push_back(nums[i]);
-                i++;
-            }else{
-                temp.push_back(nums[j]);
-                j++;
-            }
-        }
-        while(i <= mid){
-            temp.push_back(nums[i]);
+    int partition(vector<int> &arr, int low, int high) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) {
             i++;
         }
-        while(j <= high){
-            temp.push_back(nums[j++]);
-        }
-        for(int i = low; i<= high; i++){
-            nums[i] = temp[i-low];
-        }
-    }
-    void mergeSort(vector<int> &nums, int low, int high){
-        if(low >= high){
-            return ;
-        }
-        int mid = (low + high)/2;
-        mergeSort(nums, low, mid);
-        mergeSort(nums, mid+1, high);
 
-        merge(nums, low, mid, high);
+        while (arr[j] > pivot && j >= low + 1) {
+            j--;
+        }
+        if (i < j) swap(arr[i], arr[j]);
     }
-    
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void qs(vector<int> &arr, int low, int high) {
+    if (low < high) {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
     vector<int> sortArray(vector<int>& nums) {
-        
-        mergeSort(nums, 0 , nums.size()-1);
-
+        qs(nums, 0, nums.size()-1);
         return nums;
     }
 };
