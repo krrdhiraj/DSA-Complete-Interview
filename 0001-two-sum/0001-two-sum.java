03ee23b23e790a11633361043f5a48ce4981fa82
Dhiraj kumar <chiku930400@gmail.com>
2025-07-16 01:52:26 +0530

    Time: 38 ms (42.95%), Space: 45.1 MB (47.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,8 +5,7 @@ class Solution {
             for(int j = i+1; j<n; j++){
                 if(nums[i]+nums[j] == target){
                     return new int[]{i,j};
-                }else if(nums[i]+nums[j] > target)
-                    break;
+                }
             }
         }
         return new int[]{-1,-1};
