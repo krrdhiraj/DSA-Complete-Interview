631f40ad6a9ef6f29e8550ff9346820a85902653
Dhiraj kumar <chiku930400@gmail.com>
2025-07-19 00:20:59 +0530

    Time: 25 ms (2.43%), Space: 57.4 MB (20.85%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,14 +3,29 @@ class Solution {
         int sm = Integer.MIN_VALUE;
         int n = nums.length;
         int currSm = 0;
+        int startIndx = -1, endIndx = -1;
+        int start = -1;
+        List<Integer> list = new ArrayList<>();
         for(int i = 0; i<n ;i++){
+            if(currSm == 0){
+                start = i;
+            }
             currSm += nums[i];
 
-            sm = Math.max(currSm, sm);
+            if(currSm > sm){
+                sm = currSm;
+                startIndx = start;
+                endIndx = i;
+            }
             if(currSm < 0){
                 currSm = 0;
             }
         }
+        while(startIndx <= endIndx){
+            list.add(nums[startIndx]);
+            startIndx++;
+        }
+        System.out.println(list);
         return sm;
     }
 }
