14dd25832eb24aa5f1f6270c6b0afc78e66d4379
Dhiraj kumar <chiku930400@gmail.com>
2025-07-27 20:21:06 +0530

    Time: 2 ms (99.84%), Space: 50.8 MB (9.27%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,37 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        List<Integer> ans = new ArrayList<>();
         int n = nums.length;
-        for(int i = 0; i<n; i++){
-            int cnt = 1;
-            while(i+1 < n && nums[i] == nums[i+1]){
-                cnt++;
-                i++;
+        int maj = 0;
+        int cnt1 = 0, cnt2 = 0;
+        int maj1 = Integer.MIN_VALUE, maj2 = Integer.MAX_VALUE;
+        List<Integer> ans = new ArrayList<>();
+        for(int num : nums){ // Boyer Moore voting
+            if(cnt1 == 0 && maj2 != num){
+                maj1 = num;
+                cnt1 = 1;
+            }else if(cnt2 == 0 && maj1 != num){
+                maj2 = num;
+                cnt2 = 1;
+            }else if(maj1 == num){
+                cnt1++;
+            }else if(maj2 == num){
+                cnt2++;
+            }else{
+                cnt1--;
+                cnt2--;
             }
-            if(cnt > n/3)
-               ans.add(nums[i]);
-
         }
+         cnt1 = 0;cnt2 = 0;
+        for(int i = 0; i<n; i++){
+            if(nums[i] == maj1){
+                cnt1++;
+            }
+            if(nums[i] == maj2){
+                cnt2++;
+            }
+        }
+        if(cnt1 > n/3) ans.add(maj1);
+        if(cnt2 > n/3) ans.add(maj2);
         return ans;
     }
 }
