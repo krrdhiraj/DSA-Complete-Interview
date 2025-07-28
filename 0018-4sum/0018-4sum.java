7a6ab99e02a394e68c4d196578f07c45fffbe82c
Dhiraj kumar <chiku930400@gmail.com>
2025-07-29 00:41:58 +0530

    Time: 15 ms (88.03%), Space: 44.1 MB (47.35%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,31 +1,37 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-           int n = nums.length; // size of the array
-        Set<List<Integer>> st = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int i = 0; i<n; i++){
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
+            for(int j = i+1; j<n; j++){
+                if(j > i+1 && nums[j] == nums[j-1])
+                    continue;
+                
+                int left = j+1;
+                int right = n-1;
+                // now apply two pointer
+                while(left < right){
+                    long sum = (long)nums[i] + (long)nums[j] + (long)nums[left] + (long)nums[right];
 
-        // checking all possible quadruplets:
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                Set<Long> hashset = new HashSet<>();
-                for (int k = j + 1; k < n; k++) {
-                    
-                    long sum = nums[i] + nums[j];
-                    sum += nums[k];
-                    long fourth = target - sum;
-                    if (hashset.contains(fourth)) {
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[k]);
-                        temp.add((int) fourth);
-                        temp.sort(Integer::compareTo);
-                        st.add(temp);
+                    if(sum > target){
+                        right--;
+                    }else if(sum < target){
+                        left++;
+                    }else{
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while(left < right && nums[left] == nums[left+1])
+                            left++;
+                        while(left < right && nums[right] == nums[right-1])
+                            right--;
+                        left++;
+                        right--;
                     }
-                    hashset.add((long) nums[k]);
                 }
             }
         }
-        List<List<Integer>> ans = new ArrayList<>(st);
         return ans;
     }
 }
