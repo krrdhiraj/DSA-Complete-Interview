9d79e8733f78655b90bda2746cbd59304f410390
Dhiraj kumar <chiku930400@gmail.com>
2025-07-30 01:10:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] mp = new int[26];
+        for(int i = 0; i<s.length(); i++){
+            mp[s.charAt(i)-'a']++;
+        }
+        for(int j = 0; j<t.length(); j++){
+            mp[t.charAt(j)-'a']--;
+            if(mp[t.charAt(j)-'a'] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
