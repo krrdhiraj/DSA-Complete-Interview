2da77100c6299314eb5800464afed3967ec3fd48
Dhiraj kumar <chiku930400@gmail.com>
2025-08-12 00:45:31 +0530

    Time: 4 ms (36.95%), Space: 13.3 MB (78.73%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
     vector<string> ans;
-    bool isValid(string &str){
-        int cnt = 0;
-        for(char ch : str){
-            if(ch == '('){
-                cnt++;
-            }else{
-                cnt--;
-            }
-            if(cnt < 0)
-                return false;
-        }
-        return cnt == 0;
-    }
-    void solve(string& curr, int n){
+    void solve(string& curr, int n, int open, int close){
         if(curr.length() == 2*n){
-            if(isValid(curr)){
                 ans.push_back(curr);
-            }
             return;
         }
 
         // take open bracket
-        curr.push_back('(');
-        solve(curr, n);
-        curr.pop_back();
+        if(open < n){
+            curr.push_back('(');
+            solve(curr, n, open+1, close);
+            curr.pop_back();
+        }
 
         // take close bracket
-        curr.push_back(')');
-        solve(curr, n);
-        curr.pop_back();
+        if(close < open){
+            curr.push_back(')');
+            solve(curr, n, open, close+1);
+            curr.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
         string curr = "";
-        solve(curr, n);
+
+        solve(curr, n, 0, 0);
 
         return ans;
     }
