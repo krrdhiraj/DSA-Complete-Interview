440f688013e783a032f163983e38350811cc2a9a
Dhiraj kumar <chiku930400@gmail.com>
2025-08-23 01:28:30 +0530

    Time: 0 ms (100%), Space: 41.9 MB (97.11%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,17 +3,17 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        for(int i = 0; i<m; i++){
-            int l = 0, h = n-1;
-            while(l <= h){
-                int mid = (l+h)/2;
-                if(matrix[i][mid]== target){
-                    return true;
-                }else if(matrix[i][mid] > target){
-                    h = mid-1;
-                }else{
-                    l = mid+1;
-                }
+        int l = 0, h = m*n-1;
+        while(l <= h){
+            int mid = (l + h)/2;
+            int r = mid/n, c = mid%n;
+
+            if(matrix[r][c] > target){
+                h = mid-1;
+            }else if(matrix[r][c] < target){
+                l = mid+1;
+            }else{
+                return true;
             }
         }
         return false;
