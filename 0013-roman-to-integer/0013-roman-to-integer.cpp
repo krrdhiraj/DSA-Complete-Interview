fe1c29054d75fcef22304ef43ec67e0b77beb9d5
Dhiraj kumar <chiku930400@gmail.com>
2025-08-25 01:08:30 +0530

    Time: 3 ms (75.78%), Space: 13.1 MB (56.35%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<char, int> mp{{'I',1}, {'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D', 500}, {'M', 1000} };
         int ans = 0;
         for(int i = 0; i<n; i++){
-            if(s[i] == 'I' && i+1 < n && s[i+1] == 'V'){
+            if(s[i] == 'I' && i+1 < n && (s[i+1] == 'V' || s[i+1] == 'X')){
                 ans += mp[s[i+1]]-mp[s[i]];
                 i++;
             }else if(s[i] == 'X' && i+1 < n && (s[i+1] == 'L' || s[i+1] == 'C' )){
