6432b800b4c11383dd13137e7b7acabbaf46c1c5
Dhiraj kumar <chiku930400@gmail.com>
2025-08-27 01:49:19 +0530

    Time: 74 ms (76.61%), Space: 8.8 MB (91.69%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    typedef pair<char,int> p;
     int beautySum(string s) {
         int n = s.length();
 
         int ans = 0;
 
         for(int i = 0; i<n; i++){
-            unordered_map<char,int> mp;
+            int freq[26]= {0};
             for(int j = i; j < n ;j++){
-                mp[s[j]]++;
+                freq[s[j]-'a']++;
                 int mxFreq = 0, mnFreq = INT_MAX;
-                for(auto &it : mp){
-                    mxFreq = max(mxFreq, it.second);
-                    mnFreq = min(mnFreq, it.second);
+                for(int k = 0; k<26; k++){
+                    mxFreq = max(mxFreq, freq[k]);
+                    if(freq[k] != 0)
+                        mnFreq = min(mnFreq, freq[k]);
                 }
                 ans += mxFreq - mnFreq;
             }
-            
         }
         return ans;
     }
