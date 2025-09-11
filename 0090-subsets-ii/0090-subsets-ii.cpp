b19088c0a7c2f3781098ffb76442e9c7d408b22a
Dhiraj kumar <chiku930400@gmail.com>
2025-09-12 00:13:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void recursion(vector<int>& nums, int idx, vector<int>& temp, vector<vector<int>>& ans){
+        if(idx >= nums.size()){
+            if(find(begin(ans), end(ans), temp) == ans.end())
+                ans.push_back(temp);
+            return ;
+        }
+        
+        temp.push_back(nums[idx]);
+        recursion(nums, idx+1, temp, ans);
+        temp.pop_back();
+        recursion(nums, idx+1, temp, ans);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        // sort(begin(nums), end(nums));
+        recursion(nums, 0, temp, ans);
+
+        return ans;
+    }
+};
