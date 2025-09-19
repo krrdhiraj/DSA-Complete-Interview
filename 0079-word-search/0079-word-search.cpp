aa9f7565ca6c522e8ed04094ee082d726564a237
Dhiraj kumar <chiku930400@gmail.com>
2025-09-20 00:53:37 +0530

    Time: 813 ms (27.77%), Space: 11.6 MB (44.46%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     int m, n;
-    vector<vector<int>> dir{{0,1}, {1,0}, {-1,0}, {0,-1}};
-    bool solve(vector<vector<char>>&board, int i, int j, int idx, string word){
-        if(idx == word.length()){
+    int dir[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+    bool backTrack(vector<vector<char>> &board, int i, int j, int idx, string word){
+        if(idx == word.length())
             return true;
-        }
-        if(i >= m || j >= n || i < 0 || j < 0 || board[i][j] == '&'){
-            return false;
-        }
-        if(board[i][j] != word[idx])
+        if(i < 0 || j < 0 || i >= m || j>= n || board[i][j] == '&' || board[i][j] != word[idx])
             return false;
         char ch = board[i][j];
         board[i][j] = '&';
-        for(vector<int> d : dir){
-            int newI = i + d[0];
-            int newJ = j + d[1];
-            if(solve(board, newI, newJ, idx+1, word)){
+
+        for(int k = 0; k<4; k++){
+            int newI = i + dir[k][0];
+            int newJ = j + dir[k][1];
+            if(backTrack(board, newI, newJ, idx+1, word))   
                 return true;
-            }
         }
-        board[i][j] = ch;
+        board[i][j] = ch; // backtrack :- reverse the changes
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        m = board.size();
-        n = board[0].size();
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(board[i][j] == word[0]){
-                    if(solve(board, i, j, 0, word))
-                        return true;
-                }
+       m = board.size(); n = board[0].size();
+
+       for(int i = 0; i< m ; i++){
+        for(int j = 0; j < n; j++){
+            if(board[i][j] == word[0]){
+                if(backTrack(board, i, j, 0, word))
+                    return true;
             }
         }
-        return false;
+       }
+       return false; 
     }
 };
