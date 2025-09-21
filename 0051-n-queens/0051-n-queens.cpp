6dac683427808a5ce34ed939a7585c0accb4ab21
Dhiraj kumar <chiku930400@gmail.com>
2025-09-21 09:22:57 +0530

    Time: 11 ms (15.99%), Space: 12.2 MB (21.27%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     vector<vector<string>> res;
-    bool isSafe(vector<string>& board, int row, int col, int n){
-        // check upward col
-        for(int i = row-1; i >= 0; i--){
-            if(board[i][col] == 'Q')
-                return false;
-        }
-        // check left diagonal
-        for(int i = row-1, j=col-1; j >= 0 && i >= 0; j--,i--){
-            if(board[i][j] == 'Q')
-                return false;
-        }
-        // check right diagonal
-        for(int i = row-1, j=col+1; j < n && i >= 0; j++,i--){
-            if(board[i][j] == 'Q')
-                return false;
-        }
-        return true;
-    }
+    unordered_set<int> colSt;
+    unordered_set<int> diagSt;
+    unordered_set<int> antiDiagSt;
+    
     void placeQueens(int n, int row, vector<string> &board){
         if(row >= n){
             res.push_back(board);
             return ;
         }
         for(int col = 0; col<n; col++){
-            if(isSafe(board, row, col, n)){
-                board[row][col] = 'Q';
-                placeQueens(n, row+1, board);
-                board[row][col] = '.';
+            int diagonal = row + col;
+            int antiDiag = row - col;
+            if(colSt.find(col) != colSt.end() 
+            || diagSt.find(diagonal) != diagSt.end() 
+            || antiDiagSt.find(antiDiag) != antiDiagSt.end()){
+                continue;
             }
+            colSt.insert(col);
+            diagSt.insert(diagonal);
+            antiDiagSt.insert(antiDiag);
+            board[row][col] = 'Q';
+            placeQueens(n, row+1, board);
+            board[row][col] = '.';
+            colSt.erase(col);
+            diagSt.erase(diagonal);
+            antiDiagSt.erase(antiDiag);
         }
     }
     vector<vector<string>> solveNQueens(int n) {
