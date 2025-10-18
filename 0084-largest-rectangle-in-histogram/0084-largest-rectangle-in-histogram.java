c68e03a46d51504cd62787e970570886cb3969f5
Dhiraj kumar <chiku930400@gmail.com>
2025-10-18 23:09:02 +0530

    Time: 72 ms (48%), Space: 57.5 MB (64.33%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,40 +1,18 @@
 class Solution {
-    int n;
-    public void pse(int[] heights, int[] pse){
-        Stack<Integer> st = new Stack<>();
-        pse[0] = -1;
-        for(int i = 0; i<n; i++){
-            while(!st.empty() && heights[st.peek()] >= heights[i]){
-                st.pop();
-            }
-            pse[i] = st.size() == 0 ? -1 : st.peek();
-            st.push(i);
-        }
-        
-    }
-    public void nse(int[] heights, int[] nse){
-        Stack<Integer> st = new Stack<>();
-        nse[n-1] = n;
-        for(int i = n-1; i>= 0; i--){
-            while(!st.empty() && heights[st.peek()] >= heights[i]){
-                st.pop();
-            }
-            nse[i] = st.size() == 0 ? n : st.peek();
-            st.push(i);
-        }
-        
-    }
+    
     public int largestRectangleArea(int[] heights) {
-        n = heights.length;
+        int n = heights.length;
         int mxArea = 0;
-        int[] nseA = new int[n];
-        int[] pseA = new int[n];
-        nse(heights, nseA);
-        pse(heights, pseA);
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0 ; i<=n; i++){
+           while(!st.empty() && (i == n || heights[st.peek()] >= heights[i])){
+                int height = heights[st.peek()];
+                st.pop();
+                int width = st.size() == 0 ? i : i - st.peek()-1;
 
-        for(int i = 0 ; i<n; i++){
-           
-            mxArea = Math.max(mxArea, (heights[i] * (nseA[i]-pseA[i]-1)));
+                mxArea = Math.max(mxArea, (height * width));
+           }
+           st.push(i);
         }
         return mxArea;
     }
