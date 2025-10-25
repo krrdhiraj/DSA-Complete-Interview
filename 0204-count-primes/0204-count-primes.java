08209c41fd42a0abc28412ceff37ff65c4dcad56
Dhiraj kumar <chiku930400@gmail.com>
2025-10-25 16:48:21 +0530

    Time: 107 ms (26.21%), Space: 64.3 MB (10.08%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -4,21 +4,26 @@ class Solution {
         int count = 0;
         int[] primes = new int[n+1];
 
+        // O(N)
         for(int i = 2; i<= n; i++){
             primes[i] = 1;
         }
 
-        for(int i = 2; i <= n; i++){
+        // TC -> O(Nlog(log(N)))
+        for(int i = 2; i*i <= n; i++){ // sqrt(N)
             if(primes[i] == 1){
-                for(int j = i*2; j<= n; j+= i){
+                for(int j = i*i; j<= n; j+= i){
                     primes[j] = 0;
                 }
             }
         }
+
+        // O(N)
         for(int i = 2; i< n; i++){
             if(primes[i] == 1)
                 count++;
         }
+        // total TC = O(2N) + Nlog(logN)
         return count;
     }
 }
