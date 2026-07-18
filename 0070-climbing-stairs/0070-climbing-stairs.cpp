56e8eb85a0de79faa0c58a1e4cd125414aa452b8
Dhiraj kumar <chiku930400@gmail.com>
2026-07-18 11:36:20 +0530

    Time: 0 ms (100%), Space: 8.8 MB (20.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int memoization(int n, vector<int>& dp){
-        if(n <= 2)
-            return n;
-        if(dp[n] != -1)
-            return dp[n];
-        return dp[n] = memoization(n-1, dp) + memoization(n-2, dp);
-    }
+    
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return memoization(n,dp);
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i<= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
