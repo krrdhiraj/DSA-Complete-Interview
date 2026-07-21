893d396cd26f0722982c2b1693b274ad3b2b4e91
Dhiraj kumar <chiku930400@gmail.com>
2026-07-21 22:27:26 +0530

    Time: 3 ms (8.35%), Space: 17.4 MB (97.35%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -5,12 +5,14 @@ public:
         int n = cost.size();
         if(n < 2)
             return 0;
-        vector<int> dp(n+1); 
+        vector<int> dp(3); 
         dp[0] = 0;
         dp[1] = 0;
         for(int i = 2; i<= n; i++){
-            dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]);
+            dp[2] = min(dp[1]+cost[i-1], dp[0]+cost[i-2]);
+            dp[0] = dp[1];
+            dp[1] = dp[2];
         }
-        return dp[n];
+        return dp[2];
     }
 };
